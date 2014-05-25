88c26d5ee3d22477bc6d0db53aae4673f759b71b
pepers <matt.pepers@gmail.com>
2014-05-25 15:04:43 -0400

    planned out functions

---
+++ pe006.py
@@ -0,0 +1,16 @@
+# Solution to http://projecteuler.net/problem=6 by Matthew Pepers
+
+
+#returns the sum of the squares of the first n natural numbers
+def sum_of_squares(n):
+    pass
+
+#returns the square of the sum of the first n natural numbers   
+def square_of_sum(n):
+    pass
+
+
+#Test:
+#returns the difference between the sum of the squares and the square of the sum of the first 100 natural numbers
+n = 100
+return sum_of_squares(n) - square_of_sum(n)
