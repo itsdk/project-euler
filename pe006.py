9276e638c1115114b6e0e511e21b64032865b560
pepers <matt.pepers@gmail.com>
2014-05-25 15:17:29 -0400

    finished problem

--- pe006.py
+++ pe006.py
@@ -3,14 +3,20 @@
 
 #returns the sum of the squares of the first n natural numbers
 def sum_of_squares(n):
-    pass
+    numbers = 0
+    for i in range(1, n+1):
+        numbers += i**2
+    return numbers
 
 #returns the square of the sum of the first n natural numbers   
 def square_of_sum(n):
-    pass
+    numbers = 0
+    for i in range(1, n+1):
+        numbers += i
+    return numbers**2
 
 
 #Test:
-#returns the difference between the sum of the squares and the square of the sum of the first 100 natural numbers
+#prints the difference between the sum of the squares and the square of the sum of the first 100 natural numbers
 n = 100
-return sum_of_squares(n) - square_of_sum(n)
+print square_of_sum(n) - sum_of_squares(n)
