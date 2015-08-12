cc5b83424648b2159c59b725d9ad667e7df9740d
pepers <matt.pepers@gmail.com>
2015-08-12 17:06:41 -0400

    finished using Sieve of Eratosthenes

---
+++ pe007.hs
@@ -0,0 +1,17 @@
+{- 
+
+Project Euler problem #7 (https://projecteuler.net/problem=7)
+
+By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
+What is the 10 001st prime number?
+
+-}
+
+main = print $ getPrime 10001
+
+getPrime :: Int -> Int
+getPrime x = primeSieve [2..] !! (x - 1)
+
+primeSieve :: Integral a => [a] -> [a]
+primeSieve (x:xs) = x : primeSieve (filter ((/= 0) . (`mod` x)) xs)
+
